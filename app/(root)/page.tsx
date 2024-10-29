87fc724c4c4d5f346cd62702bbfc2477feb106ca
AmitBiswas1402 <amit.142biswas@gamil.com>
2024-10-29 20:00:29 +0530

    sanity schema made

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default async function Home({searchParams}: {
-  searchParams: Promise<{query ?: string}>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
 }) {
-  const query = (await searchParams).query
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      description: "This is a description",
+      image: "https://images.unsplash.com/photo-1507146153580-69a1fe6d8aa1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cm9ib3QlMjBjb21wYW55fGVufDB8fDB8fHww",
+      category: "Robots",
+      title: "We robots are here",
+    },
+  ];
 
   return (
     <>
@@ -16,9 +31,24 @@ export default async function Home({searchParams}: {
         </p>
 
         <SearchForm query={query} />
-
       </section>
 
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Latest pitches"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+
+      </section>
     </>
   );
 }
